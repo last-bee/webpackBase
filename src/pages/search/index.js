1779f958871ab1b084617e3c9574686cf7706e57
lastbee <lastbee@163.com>
2019-07-05 10:46:22 +0800

    npm pulish

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import meinv from './images/meinv.jpg'
+import addNumber from 'lastbee-add-number'
 import './index.css'
 import './index.scss'
 // import { common } from '../../common/common.js'
@@ -22,7 +23,9 @@ class Search extends React.Component {
   }
   render() {
     const { Text } = this.state;
+    const number = addNumber('99999', '1')
     return <div className="search-text">
+      { number }
       {
         Text? <Text /> : null
       }
